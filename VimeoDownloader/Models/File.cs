120a7b27d70e99d4ceab5b98b10a8d5469844152
CLCLIENT02\Rob <rob@codefabriek.nl>
2016-12-15 14:11:31 +0100

    Add project files.

---
+++ VimeoDownloader/Models/File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VimeoDownloader.Models
+{
+    /// <summary>
+    /// Class File.
+    /// </summary>
+    public class File
+    {
+        /// <summary>
+        /// Gets or sets the progressive.
+        /// </summary>
+        /// <value>The progressive.</value>
+        public IEnumerable<Progressive> Progressive { get; set; }
+
+        /// <summary>
+        /// Sorts this instance.
+        /// </summary>
+        internal void Sort()
+        {
+            Progressive = Progressive.OrderByDescending(x => x.Height);
+        }
+    }
+}
---
+++ VimeoDownloader/VimeoException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VimeoDownloader
+{
+    /// <summary>
+    /// Class VimeoDownloadException.
+    /// </summary>
+    /// <seealso cref="System.ApplicationException" />
+    public class VimeoException : ApplicationException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VimeoException"/> class.
+        /// </summary>
+        /// <param name="message">A message that describes the error.</param>
+        public VimeoException(string message)
+            : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="VimeoException"/> class.
+        /// </summary>
+        /// <param name="videoId">The video identifier.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public VimeoException(string videoId, Exception innerException)
+            : base($"We are unable to download the Vimeo video {videoId}. Please check the inner exception for more details.", innerException) { }
+    }
+}
